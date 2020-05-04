7cb76b92105e64ceaff1798fb539c6c1ee667bbf
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-04 18:43:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,13 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string != string.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase
+  if string == string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
-    
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
