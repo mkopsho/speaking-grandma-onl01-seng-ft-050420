e86f60470e2610d4e7601494d871a064f191ee3a
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-04 18:41:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string != string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif string == string.upcase
     return ""
 end
