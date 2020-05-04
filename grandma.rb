53c989abda9bf0060efb895b400010930b7628f0
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-04 18:43:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,8 @@ def speak_to_grandma(string)
   elsif string == string.upcase
     return "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
-    return 
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    
+  end
 end
